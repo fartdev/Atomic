a666eb8f1ed410ca20cbd1090f291e0fe18cc23c
0x150 <constantindiscord@gmail.com>
2021-08-04 21:30:07 +0200

    mmm
    Signed-off-by: 0x150 <constantindiscord@gmail.com>

--- src/main/java/me/zeroX150/atomic/helper/AttackManager.java
+++ src/main/java/me/zeroX150/atomic/helper/AttackManager.java
@@ -1,14 +1,15 @@
 package me.zeroX150.atomic.helper;
 
+import me.zeroX150.atomic.Atomic;
 import net.minecraft.entity.LivingEntity;
 
 public class AttackManager {
-    public static final long MAX_ATTACK_TIMEOUT = 5000;
+    public static final long MAX_ATTACK_TIMEOUT = 7000;
     static long lastAttack = 0;
     static LivingEntity lastAttacked;
 
     public static LivingEntity getLastAttackInTimeRange() {
-        if (lastAttack + MAX_ATTACK_TIMEOUT < System.currentTimeMillis()) {
+        if (lastAttack + MAX_ATTACK_TIMEOUT < System.currentTimeMillis() || lastAttacked.getPos().distanceTo(Atomic.client.player.getPos()) > 64 || lastAttacked.isDead() || Atomic.client.player.isDead()) {
             lastAttacked = null;
         }
         return lastAttacked;
